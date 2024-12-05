af61db688d055d795e7dfd0ebe2a4d3e6f66ae75
Zilenc3 <alex.dwayne225@gmail.com>
2024-12-04 20:52:50 -0500

    1

--- leetCode1.py
+++ leetCode1.py
@@ -1,24 +1,25 @@
-
+maxCount = 1
 a = "abcabcbb" #3 abc
 b = "bbbbbb" #1
 c = "pwwkew" #3 wke
 
-def EvalStr (str, x, y, maxCount):
+def EvalStr (str, x, y):
+    global maxCount
     for i in range(len(str[x:y])):
         if str[-1] not in str[x:(y-1)]:
             y = y + 1
-            EvalStr(str,x,y,maxCount)
+            EvalStr(str,x,y)
         else:
             if maxCount < y:
-                maxCount = y
+                maxCount = y - 1
                 print(maxCount)
                 return
 
 
 def SearchStr (testStr):
-    maxCount = 1
     for i in range(len(testStr)):
-        partialStr = testStr[i:len(testStr)]
-        EvalStr(partialStr,1,2, maxCount)
+        for j in range(len(testStr) - i):
+            partialStr = testStr[i:j+2]
+            EvalStr(partialStr,1,2)
 
 SearchStr(a)
